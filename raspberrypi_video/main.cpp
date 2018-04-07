ef8e25c46d7b868cd468bfd7380c4ebb4b15d53c
tlemarec <titouan-lemarec@outlook.com>
2018-04-07 19:22:31 +0200

    sdgfsdfgf

--- raspberrypi_video/main.cpp
+++ raspberrypi_video/main.cpp
@@ -25,13 +25,13 @@ int main( int argc, char **argv )
 	//fill the top left corner with red, just bcuz
 	QImage myImage;
 	myImage = QImage(320, 240, QImage::Format_RGB888);
-	QRgb red = qRgb(255,0,0);
+	/*QRgb red = qRgb(255,0,0);
 	for(int i=0;i<80;i++) {
 		for(int j=0;j<60;j++) {
 			myImage.setPixel(i, j, red);
 		}
 	}
-	//ss
+	*/
 
 	//create a label, and set it's image to the placeholder
 	MyLabel myLabel(myWidget);
